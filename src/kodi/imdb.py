713337a2657590b1a2e814e59c0ce9888cfc981a
Daniel Quinn <code@danielquinn.org>
2025-04-21 18:08:26 +0100

    Simplify actor IMDB consumption

--- src/kodi/imdb.py
+++ src/kodi/imdb.py
@@ -132,10 +132,9 @@ def generate_imdb(id, language="en", fanart="none", fanart_file="folder.jpg", pa
                     add_node(doc, root, "genre", genre)
             else:
                 add_node(doc, root, "genre", j["genre"])
-        if "actor" in j:
-            for actor in j["actor"]:
-                xactor = add_node(doc, root, "actor")
-                add_node(doc, xactor, "name", actor["name"])
+        for actor in j.get("actor", ()):
+            xactor = add_node(doc, root, "actor")
+            add_node(doc, xactor, "name", actor["name"])
         if "trailer" in j and "embedUrl" in j["trailer"]:
             add_node(doc, root, "trailer", "https://www.imdb.com" + j["trailer"]["embedUrl"])
         if "aggregateRating" in j and "ratingValue" in j["aggregateRating"]:
